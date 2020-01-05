9534e406a6bb4e41ff47d9be773a5deee0830022
mrliu <1343926437@qq.com>
2020-01-05 11:30:54 +0800

    Test HelloWorld

---
+++ TestHelloWorld/src/main/java/com/mrliu/TestHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.mrliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestHelloWorldApplication.class, args);
+	}
+
+}
