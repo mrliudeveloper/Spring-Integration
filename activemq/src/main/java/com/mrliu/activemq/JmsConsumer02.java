7288abf74f7ab7510b741d9843af55a3f3f310e2
mrliu <1343926437@qq.com>
2020-01-09 16:05:54 +0800

    add consumer listener

--- activemq/src/main/java/com/mrliu/activemq/JmsConsumer02.java
+++ activemq/src/main/java/com/mrliu/activemq/JmsConsumer02.java
@@ -1,19 +1,22 @@
 package com.mrliu.activemq;
 
+import java.io.IOException;
+
 import javax.jms.Connection;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Queue;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.activemq.ActiveMQXAConnectionFactory;
 
-public class JmsConsumer {
+public class JmsConsumer02 {
 	public static final String ACTIVEMQ_URL="tcp://192.168.179.4:61616";
 	public static final String QUEUE_NAME="queue";
-	public static void main(String[] args) throws JMSException {
+	public static void main(String[] args) throws JMSException, IOException {
 		//1.创建连接工厂,按照给定的url地址，采用默认用户名和密码
 				ActiveMQXAConnectionFactory activeMQXAConnectionFactory 
 				= new ActiveMQXAConnectionFactory("admin", "admin", ACTIVEMQ_URL);
@@ -28,16 +31,24 @@ public class JmsConsumer {
 				Queue queue=session.createQueue(QUEUE_NAME);
 				//6.创建消费者
 				MessageConsumer messageConsumer = session.createConsumer(queue);
-				while (true) {
-					//7.消费消息的两种方式
-					//TextMessage textMessage =(TextMessage)messageConsumer.receive();
-					TextMessage textMessage =(TextMessage)messageConsumer.receive(4000L);
-					if (textMessage!=null) {
-						System.out.println("接收到的消息为"+textMessage.getText());
-					}else {
-						break;
+				//7.创建消息监听器
+				messageConsumer.setMessageListener(new MessageListener() {
+					
+					@Override
+					public void onMessage(Message message) {
+						// TODO Auto-generated method stub
+						if (message!=null && message instanceof TextMessage) {
+							TextMessage textMessage=(TextMessage)message;
+							try {
+								System.out.println("消费者接收到信息："+textMessage.getText());
+							} catch (JMSException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
 					}
-				}
+				});
+				System.in.read();//保证控制台不灭
 				//8.关闭资源
 				messageConsumer.close();
 				session.close();
